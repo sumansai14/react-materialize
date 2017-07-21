69f48ebdcd51ef6083586c8d206ce7270da24aa2
Alexander Odell <alextrastero@gmail.com>
2017-07-21 12:08:06 +0200

    BreadcrumbsPage

--- docs/src/pages/BreadcrumbsPage.js
+++ docs/src/pages/BreadcrumbsPage.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import Row from 'Row';
+import Section from 'Section';
 import Col from 'Col';
 import ReactPlayground from './ReactPlayground';
 import PropTable from './PropTable';
-import Samples from './Samples';
-import breadcrumbs from '../../../examples/Breadcrumbs';
 import BreadcrumbsCode from '!raw-loader!Breadcrumb';
 
 const header = 'Breadcrumbs';
+const desc = `Breadcrumbs are a good way to display your current location.
+  This is usually used when you have multiple layers of content.`;
 
 const BreadcrumbsPage = () => (
   <Row>
-    <Col m={9} s={12} l={10}>
-      <p className='caption'>
-        Breadcrumbs are a good way to display your current location. This is usually used when you have multiple layers of content.
-      </p>
-      <h4 className='col s12'>Basic</h4>
-      <Col s={12}>
-        <ReactPlayground code={Samples.breadcrumb}>
-          {breadcrumbs}
-        </ReactPlayground>
+    <Col s={12}>
+      <Section>
+        <p className='caption'>{desc}</p>
+        <h4>Basic</h4>
+        <ReactPlayground code={require('!raw-loader!../../../examples/Breadcrumbs.js')} />
+      </Section>
+
+      <Section>
         <PropTable header={header} component={BreadcrumbsCode} />
-      </Col>
+      </Section>
     </Col>
   </Row>
 );
