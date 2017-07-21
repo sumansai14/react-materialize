6e0130b00940c2f9bf536f4406c88ccff981bd6a
Alexander Odell <alextrastero@gmail.com>
2017-07-21 14:51:43 +0200

    CarouselPage

--- docs/src/pages/CarouselPage.js
+++ docs/src/pages/CarouselPage.js
@@ -7,31 +7,31 @@ import PropTable from './PropTable';
 import CarouselCode from '!raw-loader!Carousel';
 
 const header = 'Carousel';
-const desc = `Our Carousel is a robust and versatile component that can be an image slider,
-  to an item carousel, to an onboarding experience.
+const desc = `Our Carousel is a robust and versatile component that can be an
+  image slider, to an item carousel, to an onboarding experience.
   It is touch enabled making it especially smooth to use on mobile.`;
 
 const CarouselPage = () => (
   <Row>
-    <Col s={12} l={10}>
+    <Col s={12}>
       <Section>
         <p className='caption'>{desc}</p>
-        <h4 className='col s12'>Image Carousel</h4>
+        <h4>Image Carousel</h4>
         <ReactPlayground code={require('!raw-loader!../../../examples/CarouselBasic.js')} />
       </Section>
 
       <Section>
-        <h4 className='col s12'>Full Width Slider</h4>
+        <h4>Full Width Slider</h4>
         <ReactPlayground code={require('!raw-loader!../../../examples/CarouselFullWidth.js')} />
       </Section>
 
       <Section>
-        <h4 className='col s12'>Full Width Slider with Fixed Item</h4>
+        <h4>Full Width Slider with Fixed Item</h4>
         <ReactPlayground code={require('!raw-loader!../../../examples/CarouselFullWidthFixedItem.js')} />
       </Section>
 
       <Section>
-        <h4 className='col s12'>Content Carousel</h4>
+        <h4>Content Carousel</h4>
         <ReactPlayground code={require('!raw-loader!../../../examples/CarouselContent.js')} />
       </Section>
 
