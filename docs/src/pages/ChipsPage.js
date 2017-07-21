d671d478d4fd4ec401f8c8ffa96c9c7d6b11e2fd
Alexander Odell <alextrastero@gmail.com>
2017-07-21 17:27:02 +0200

    ChipsPage

--- docs/src/pages/ChipsPage.js
+++ docs/src/pages/ChipsPage.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import Row from 'Row';
 import Col from 'Col';
+import Section from 'Section';
 import ReactPlayground from './ReactPlayground';
 import PropTable from './PropTable';
-import Samples from './Samples';
-import chips from '../../../examples/Chips';
 import Code from '!raw-loader!Chip';
+
+const header = 'Chip';
+const desc = `Chips can be used to represent small blocks of information.
+  They are most commonly used either for contacts or for tags.`;
 
 const ChipsPage = () => (
   <Row>
-    <Col m={9} s={12} l={10}>
-      <p className='caption'>
-        Chips can be used to represent small blocks of information. They are most commonly used either for contacts or for tags.
-      </p>
-      <Col s={12}>
-        <ReactPlayground code={Samples.chips}>
-          {chips}
-        </ReactPlayground>
-      </Col>
+    <Col s={12}>
+      <Section>
+        <p className='caption'>{desc}</p>
+        <h4>Basic</h4>
+        <ReactPlayground code={require('!raw-loader!../../../examples/Chips.js')} />
+      </Section>
 
-      <Col s={12}>
-        <PropTable header='Chip' component={Code} />
-      </Col>
+      <Section>
+        <PropTable header={header} component={Code} />
+      </Section>
     </Col>
   </Row>
 );
