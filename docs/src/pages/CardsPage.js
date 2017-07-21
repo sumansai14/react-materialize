f23e51178d433ce05b39c8f6d59aec7a3092a5bb
Alexander Odell <alextrastero@gmail.com>
2017-07-21 14:50:52 +0200

    CardsPage

--- docs/src/pages/CardsPage.js
+++ docs/src/pages/CardsPage.js
@@ -1,60 +1,49 @@
 import React from 'react';
 import Row from 'Row';
 import Col from 'Col';
+import Section from 'Section';
 import ReactPlayground from './ReactPlayground';
 import PropTable from './PropTable';
-import Samples from './Samples';
 import CardCode from '!raw-loader!Card';
 
-import imageCard from '../../../examples/CardImage';
-import revealCard from '../../../examples/CardReveal';
-import panelCard from '../../../examples/CardPanel';
-import simpleCard from '../../../examples/CardSimple';
+const header = 'Cards';
+const desc = `Cards are a convenient means of displaying content composed
+  of different types of objects. They’re also well-suited for presenting
+  similar objects whose size or supported actions can vary considerably,
+  like photos with captions of variable length.`;
 
 const CardsPage = () => (
   <Row>
-    <Col m={9} s={12} l={10}>
-      <p className='caption'>
-        Cards are a convenient means of displaying content composed of different types of objects. They’re also well-suited for presenting similar objects whose size or supported actions can vary considerably, like photos with captions of variable length.
-      </p>
-      <h4 className='col s12'>
-        Basic Card
-      </h4>
-      <Col s={12}>
-        <ReactPlayground code={Samples.cardSimple}>
-          {simpleCard}
-        </ReactPlayground>
-      </Col>
+    <Col s={12}>
+      <Section>
+        <p className='caption'>{desc}</p>
+        <h4>Basic Card</h4>
+        <ReactPlayground code={require('!raw-loader!../../../examples/CardSimple.js')} />
+      </Section>
 
-      <h4 className='col s12'>
-        Image Card
-      </h4>
-      <Col s={12}>
-        <ReactPlayground code={Samples.cardImage}>
-          {imageCard}
-        </ReactPlayground>
-      </Col>
+      <Section>
+        <h4>Image Card</h4>
+        <ReactPlayground code={require('!raw-loader!../../../examples/CardImage.js')} />
+      </Section>
 
-      <h4 className='col s12'>
-        Card Reveal
-      </h4>
-      <Col s={12}>
-        <ReactPlayground code={Samples.cardReveal}>
-          {revealCard}
-        </ReactPlayground>
-      </Col>
+      <Section>
+        <h4>Card Reveal</h4>
+        <ReactPlayground code={require('!raw-loader!../../../examples/CardReveal.js')} />
+      </Section>
 
-      <h4 className='col s12'>
-        Card Panel
-      </h4>
-      <Col s={12}>
-        <ReactPlayground code={Samples.cardPanel}>
-          {panelCard}
-        </ReactPlayground>
-      </Col>
-      <Col s={12}>
-        <PropTable header='Cards' component={CardCode} />
-      </Col>
+      <Section>
+        <h4>Card Reveal</h4>
+        <ReactPlayground code={require('!raw-loader!../../../examples/CardReveal.js')} />
+      </Section>
+
+      <Section>
+        <h4>Card Panel</h4>
+        <ReactPlayground code={require('!raw-loader!../../../examples/CardPanel.js')} />
+      </Section>
+
+      <Section>
+        <PropTable header={header} component={CardCode} />
+      </Section>
     </Col>
   </Row>
 );
