e68602cceac6ae15a818a96a4527457d41a63016
Alexander Odell <alextrastero@gmail.com>
2017-09-20 17:34:28 +0200

    Add exports to index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,56 @@
+export { default as Autocomplete } from './Autocomplete';
 export { default as Badge } from './Badge';
+export { default as Breadcrumb } from './Breadcrumb';
+export { default as Button } from './Button';
+export { default as Card } from './Card';
+export { default as CardPanel } from './CardPanel';
+export { default as CardTitle } from './CardTitle';
+export { default as Carousel } from './Carousel';
+export { default as Chip } from './Chip';
+export { default as Col } from './Col';
+export {
+  default as Collapsible,
+  CollapsibleItem
+} from './Collapsible';
+export {
+  default as Collection,
+  CollectionItem
+} from './Collection';
+export { default as Container } from './Container';
+export { default as Divider } from './Divider';
+export { default as Dropdown } from './Dropdown';
+export { default as Footer } from './Footer';
+export { default as Icon } from './Icon';
+export { default as Input } from './Input';
+export { default as MediaBox } from './MediaBox';
+export { default as MenuItem } from './MenuItem';
+export { default as Modal } from './Modal';
+export { default as Navbar } from './Navbar';
+export { default as NavItem } from './NavItem';
+export {
+  default as OverlayTrigger,
+  Overlay
+} from './OverlayTrigger';
+export {
+  default as Pagination,
+  PaginationButton
+} from './Pagination';
+export { default as Parallax } from './Parallax';
+export { default as Preloader } from './Preloader';
+export { default as ProgressBar } from './ProgressBar';
+export { default as Row } from './Row';
+export { default as SearchForm } from './SearchForm';
+export { default as Section } from './Section';
+export {
+  default as SideNav,
+  SideNavItem
+} from './SideNav';
+export { default as Slider } from './Slider';
+export { default as Slide } from './Slide'; // to go inside Slider?
+export { default as Spinner } from './Spinner';
+export { default as Tab } from './Tab'; // to go inside Tabs?
+export { default as Table } from './Table';
+export { default as Tabs } from './Tabs';
+export { default as Tag } from './Tag';
+export { default as Toast } from './Toast';
+export { default as UserView } from './UserView';
