476301c123991617413941ffc51dbcd5e6901966
Alexander Odell <alextrastero@gmail.com>
2017-12-21 18:22:11 +0100

    Removes prettier eslint

--- new-docs/src/pages/index.js
+++ new-docs/src/pages/index.js
@@ -1,11 +1,43 @@
 import React from 'react';
+import Link from 'gatsby-link';
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  const { edges: posts } = data.allMarkdownRemark;
+
   return (
     <div>
-      <h1>hello</h1>
+      {posts
+        .filter(post => post.node.frontmatter.title.length > 0)
+        .map(({ node: post }) => {
+          return (
+            <div key={post.id}>
+              <h1>
+                <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
+              </h1>
+              <h2>{post.frontmatter.date}</h2>
+              <p>{post.excerpt}</p>
+            </div>
+          );
+        })}
     </div>
   );
 };
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark {
+      edges {
+        node {
+          excerpt(pruneLength: 250)
+          id
+          frontmatter {
+            title
+            path
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default IndexPage;
