a1ff70d6d3823c84bcc819c9b289c508d4b5b020
Alexander Odell <alextrastero@gmail.com>
2017-12-21 18:13:39 +0100

    linting

--- new-docs/src/pages/index.js
+++ new-docs/src/pages/index.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const IndexPage = () => {
   return (
     <div>
       <h1>hello</h1>
     </div>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
